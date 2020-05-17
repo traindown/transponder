6c3283015d5075d77873d53b5c6db3710cb93ef6
Tyler <scottt2@uw.edu>
2020-05-17 16:48:57 -0600

    Basic editing

---
+++ android/app/src/main/kotlin/com/example/transponder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.transponder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+// Flutter code sample for TextField
+
+// This sample shows how to get a value from a TextField via the [onSubmitted]
+// callback.
+
+import "package:flutter/material.dart";
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = "Traindown Transponder";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: TraindownEditor(),
+    );
+  }
+}
+
+class TraindownEditor extends StatefulWidget {
+  TraindownEditor({Key key}) : super(key: key);
+
+  @override
+  _TraindownEditor createState() => _TraindownEditor();
+}
+
+class _TraindownEditor extends State<TraindownEditor> {
+  TextEditingController _controller;
+
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _addText(String addition) {
+    int start = _controller.selection.extentOffset;
+    int end = _controller.selection.extentOffset + addition.length;
+    _controller.value = _controller.value.copyWith(
+        text: _controller.text.replaceRange(start, start, addition),
+        selection: TextSelection.collapsed(offset: end));
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 40.0),
+              child: EditableText(
+                autocorrect: false,
+                autofocus: true,
+                backgroundCursorColor: Colors.blue,
+                cursorColor: Colors.red,
+                cursorWidth: 5,
+                controller: _controller,
+                enableSuggestions: false,
+                expands: true,
+                focusNode: FocusNode(),
+                scrollPadding: EdgeInsets.all(20.0),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.8),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              FlatButton(
+                child: Text("Meta"),
+                onPressed: () => _addText("\n# "),
+              ),
+              FlatButton(
+                child: Text("Colon"),
+                onPressed: () => _addText(": "),
+              ),
+              FlatButton(
+                child: Text("Note"),
+                onPressed: () => _addText("\n* "),
+              ),
+              FlatButton(
+                child: Text("Superset"),
+                onPressed: () => _addText("\n+ "),
+              ),
+              FlatButton(
+                child: Text("Date"),
+                onPressed: () => _addText("@ "),
+              ),
+            ],
+          ),
+        ]));
+  }
+}
