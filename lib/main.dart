ec3ed3d54fa6d703d861afdd5b6215b16411f4a0
Tyler Scott <tyler@greaterscott.com>
2020-06-11 08:10:15 -0600

    Session actions

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:traindown/traindown.dart';
+
+enum SessionMenuOption { delete, email }
 
 void main() => runApp(MaterialApp(home: Scaffold(body: Transponder())));
 
@@ -40,6 +43,43 @@ class _Transponder extends State<Transponder> {
         textColor: Colors.blue,
         child: Text('Add new session'),
         onPressed: () => _createSession());
+  }
+
+  Future<void> _sendEmail(int sessionIndex) async {
+    File session = _sessions[sessionIndex];
+    String body = session.readAsStringSync();
+    String subject = session.path.split('/').last ?? 'Traindown Session';
+    final Email email = Email(
+      body: body,
+      subject: subject,
+      recipients: ['tyler@greaterscott.com'],
+      attachmentPaths: [session.path],
+    );
+
+    String sendResponse;
+
+    try {
+      await FlutterEmailSender.send(email);
+      sendResponse = 'Email sent!';
+    } catch (error) {
+      sendResponse = error.toString();
+    }
+
+    return showCupertinoDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoAlertDialog(
+            title: Text('Email Status'),
+            content: Text(sendResponse),
+            actions: <Widget>[
+              FlatButton(
+                textColor: Colors.blue,
+                child: Text('Okay'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          );
+        });
   }
 
   Future<void> _showDeleteModal(int sessionIndex) async {
@@ -122,7 +162,7 @@ class _Transponder extends State<Transponder> {
       ),
       builder: (BuildContext context) {
         return Container(
-            height: MediaQuery.of(context).size.height * 0.85,
+            height: MediaQuery.of(context).size.height * 0.9,
             child: TraindownEditor(
                 content: _activeSessionContent, onChange: _writeSession),
             padding: EdgeInsets.only(top: 20.0));
@@ -137,13 +177,38 @@ class _Transponder extends State<Transponder> {
             itemBuilder: (context, index) {
               return Card(
                 child: ListTile(
-                  leading: Icon(Icons.fitness_center),
-                  onLongPress: () => _showDeleteModal(index),
                   onTap: () {
                     _activeSessionIndex = index;
                     _sessionEditor();
                   },
                   title: Text(_sessions[index].path.split('/').last),
+                  trailing: PopupMenuButton<SessionMenuOption>(
+                      elevation: 2.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      tooltip: 'Session action menu',
+                      onSelected: (SessionMenuOption action) {
+                        switch (action) {
+                          case SessionMenuOption.delete:
+                            _showDeleteModal(index);
+                            break;
+                          case SessionMenuOption.email:
+                            _sendEmail(index);
+                            break;
+                        }
+                      },
+                      itemBuilder: (BuildContext context) =>
+                          <PopupMenuEntry<SessionMenuOption>>[
+                            const PopupMenuItem<SessionMenuOption>(
+                              value: SessionMenuOption.email,
+                              child: Text('Send via email'),
+                            ),
+                            const PopupMenuItem<SessionMenuOption>(
+                              value: SessionMenuOption.delete,
+                              child: Text('Delete session'),
+                            ),
+                          ]),
                 ),
               );
             }));
