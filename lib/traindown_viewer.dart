8b02a4539344156fd8a7682f63ef7f46c32b927e
Tyler Scott <tyler@greaterscott.com>
2020-07-01 08:10:36 -0600

    Toward a viewer

--- lib/traindown_viewer.dart
+++ lib/traindown_viewer.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-//import 'package:traindown/traindown.dart';
+import 'package:traindown/traindown.dart';
 
 class TraindownViewer extends StatelessWidget {
   final String content;
+  Parser parser;
 
-  TraindownViewer({Key key, this.content}) : super(key: key);
+  TraindownViewer({Key key, this.content}) : super(key: key) {
+    parser = Parser.for_string(content);
+    // TODO: Guard this
+    parser.parse();
+  }
+
+  List<Movement> get movements => parser.movements;
+
+  Widget renderMovement(Movement movement) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(movement.name),
+          Table(
+              children: [
+                    TableRow(children: [
+                      TableCell(child: Text('Weight')),
+                      TableCell(child: Text('Reps')),
+                      TableCell(child: Text('Sets'))
+                    ])
+                  ] +
+                  movement.performances.map((p) {
+                    return TableRow(children: [
+                      TableCell(child: Text(p.load.toString())),
+                      TableCell(child: Text(p.reps.toString())),
+                      TableCell(child: Text(p.repeat.toString())),
+                    ]);
+                  }).toList())
+        ]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +45,11 @@ class TraindownViewer extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
           Expanded(
-            child: Padding(padding: EdgeInsets.all(10.0), child: Text(content)),
-          ),
+              child: ListView.builder(
+                  itemCount: movements.length,
+                  itemBuilder: (context, index) {
+                    return Card(child: renderMovement(movements[index]));
+                  }))
         ]));
   }
 }
